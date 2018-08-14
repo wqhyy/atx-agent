fd909e61713b5b305e10dc9f2a9bd7d26e3808bc
wqhyy <252475555@qq.com>
2018-08-14 20:30:14 +0800

    update build sh

--- agentbuild.sh
+++ agentbuild.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 go generate
 GOOS=linux GOARCH=arm go build -tags vfs
-adb install apks/app-xhs.apk
-adb install apks/app-xhs-test.apk
-adb install apks/keyboardservice.apk
+# app install
+# adb install apks/xhs.apk
+#  uiserver
+# adb install apks/app-xhs.apk
+# uiserver-test
+# adb install apks/app-xhs-test.apk
+# ADB输入法
+# adb install apks/keyboardservice.apk
+# GT性能组件
+# adb install apks/GT-for-red.apk
 adb push atx-agent /data/local/tmp
 adb shell chmod 755 /data/local/tmp/atx-agent
-adb shell /data/local/tmp/atx-agent -d -t 192.168.0.101:8080 -ta 1
+adb shell /data/local/tmp/atx-agent -stop
+adb shell /data/local/tmp/atx-agent -d -t 10.23.172.90:8080 -ta 1
