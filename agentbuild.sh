e81ae98b23941c2d05497d80f6b58be84ab49621
wqhyy <252475555@qq.com>
2018-09-03 15:07:53 +0800

    modify build shell

--- agentbuild.sh
+++ agentbuild.sh
@@ -4,14 +4,16 @@ GOOS=linux GOARCH=arm go build -tags vfs
 # app install
 # adb install apks/xhs.apk
 #  uiserver
-# adb install apks/app-xhs.apk
+# adb uninstall com.github.uiautomator
+adb install apks/app-xhs.apk
 # uiserver-test
-# adb install apks/app-xhs-test.apk
+# adb uninstall com.github.uiautomator.test
+adb install apks/app-xhs-test.apk
 # ADB输入法
-# adb install apks/keyboardservice.apk
+adb install apks/keyboardservice.apk
 # GT性能组件
 # adb install apks/GT-for-red.apk
 adb push atx-agent /data/local/tmp
 adb shell chmod 755 /data/local/tmp/atx-agent
 adb shell /data/local/tmp/atx-agent -stop
-adb shell /data/local/tmp/atx-agent -d -t 10.23.172.90:8080 -ta 1
+adb shell /data/local/tmp/atx-agent -d -t 10.100.164.98:8080 -ta 1
